c88079639c9afd7c4086dab30632f4121e1cf03d
Zach Sebag <zsebag@gmail.com>
2015-12-27 20:40:12 -0600

    Added basic entry component

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    './src/index.js',
+    './src/index.jsx',
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
   ],

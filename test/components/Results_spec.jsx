6993dc1c3cedbc3cd695444b7496ddd7b8a9c525
Zach Sebag <zsebag@gmail.com>
2016-01-03 23:39:44 -0600

    Added reducer for redux Added basic tests for reducer

--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -24,4 +24,29 @@ describe('Results', () => {
     expect(days).to.contain('0');
   });
 
+  it('invokes the next callback when next button is clicked', () => {
+    let nextInvoked = false;
+    const next = () => nextInvoked = true;
+
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const component = renderIntoDocument(
+      <Results pair={pair}
+               tally={Map()}
+               next={next}/>
+      );
+    Simulate.click(React.findDOMNode(component.refs.next));
+
+    expect(nextInvoked).to.equal(true);
+  });
+
+  it('renders the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Results winner='Trainspotting'
+               pair={['Trainspotting', '28 Days Later']}
+               tally={Map()} />
+    );
+    const winner = React.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
+  });
 });
